11c739bda181f31b12f81c1f10ccbc15ec3b9a7f
Tatti <tatuyazkt1@gmail.com>
2026-09-14 21:17:37 +0900

    lualineの条件付き読み込みを追加し、VSCode Neovimでの動作を制御

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,12 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+
+    -- VSCode Neovimではlualineを読み込まない
+    cond = function()
+      return not vim.g.vscode
+    end,
+
     opts = {
       options = {
         theme = "auto",
