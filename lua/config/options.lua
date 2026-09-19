197721b1a5203e5683580c4aaeb0a8f79f19f788
Tatti <tatuyazkt1@gmail.com>
2026-09-19 13:32:42 +0900

    render-markdown.nvimプラグインを追加し、バッファのライフサイクル管理機能を実装。neo-treeのバッファ操作を改善。

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,3 +18,22 @@ vim.opt.smartcase = true
 vim.opt.updatetime = 250
 
 vim.opt.clipboard = "unnamedplus"
+
+-- 通常選択のバッファは再利用し、Neo-treeのtで開いたバッファだけ保持する。
+-- buftypeが空でない特殊バッファ（Neo-tree、terminal、quickfixなど）は対象外。
+local function configure_buffer_lifecycle(bufnr)
+  if vim.bo[bufnr].buftype == "" then
+    local keep_buffer = vim.b[bufnr].neo_tree_keep_buffer == true
+    vim.bo[bufnr].bufhidden = keep_buffer and "hide" or "wipe"
+  end
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("ReuseNormalBuffers", { clear = true }),
+  callback = function(args)
+    configure_buffer_lifecycle(args.buf)
+  end,
+})
+
+-- 起動時に既に存在する[No Name]にも適用する。
+configure_buffer_lifecycle(0)
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,67 @@
+local function focus_target_window(state)
+  local utils = require("neo-tree.utils")
+  local target_win = utils.get_appropriate_window(state)
+  if not vim.api.nvim_win_is_valid(target_win) then
+    return nil
+  end
+  vim.api.nvim_set_current_win(target_win)
+  return target_win
+end
+
+local function set_buffer_persistent(bufnr, persistent)
+  if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].buftype == "" then
+    vim.b[bufnr].neo_tree_keep_buffer = persistent
+    vim.bo[bufnr].bufhidden = persistent and "hide" or "wipe"
+  end
+end
+
+-- 現在のバッファを置き換えて開く。
+local function open_in_current_buffer(state)
+  local node = state.tree:get_node()
+  if not node or node.type ~= "file" then
+    state.commands.open(state)
+    return
+  end
+
+  local target_win = focus_target_window(state)
+  if not target_win then
+    return
+  end
+
+  local previous_buf = vim.api.nvim_win_get_buf(target_win)
+  local was_persistent = vim.b[previous_buf].neo_tree_keep_buffer == true
+  set_buffer_persistent(previous_buf, false)
+
+  local opened, err = pcall(state.commands.open, state)
+  if not opened then
+    set_buffer_persistent(previous_buf, was_persistent)
+    error(err, 0)
+  end
+
+  local current_buf = vim.api.nvim_get_current_buf()
+  local keep_current = was_persistent or vim.b[current_buf].neo_tree_keep_buffer == true
+  set_buffer_persistent(current_buf, keep_current)
+end
+
+-- 現在のバッファを残して同じタブページ内で開く。
+local function open_in_new_buffer(state)
+  local node = state.tree:get_node()
+  if not node or node.type ~= "file" then
+    state.commands.open(state)
+    return
+  end
+
+  local target_win = focus_target_window(state)
+  if not target_win then
+    return
+  end
+
+  local previous_buf = vim.api.nvim_win_get_buf(target_win)
+  set_buffer_persistent(previous_buf, true)
+  state.commands.open(state)
+  set_buffer_persistent(vim.api.nvim_get_current_buf(), true)
+end
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -31,7 +95,14 @@ return {
           -- splitで開く
           ["<leader>sv"] = "open_vsplit",
           ["<leader>sh"] = "open_split",
-          
+
+          -- 現在のバッファを残して同じタブページ内の新しいバッファで開く
+          ["t"] = open_in_new_buffer,
+
+          -- 通常選択は現在のバッファを置き換える
+          ["<cr>"] = open_in_current_buffer,
+          ["<2-LeftMouse>"] = open_in_current_buffer,
+
           -- 競合キーを無効化
           ["s"] = "none",
           ["S"] = "none",
